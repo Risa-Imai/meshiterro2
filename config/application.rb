d3c491311679952fefc3b1709e2daa0ca6ecd56c
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-07-17 00:33:59 +0000

    update for config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Meshiterro2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.generators.template_engine = :slim
+    config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
     #
